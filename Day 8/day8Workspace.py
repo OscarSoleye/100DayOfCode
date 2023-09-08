9120473f2403cbb7b63bbbbd897cf6fbc3b98f6c
OscarSoleye <clouddavinci@gmail.com>
2023-09-08 19:14:17 +0100

    Day 8

--- Day 8/day8Workspace.py
+++ Day 8/day8Workspace.py
@@ -1,25 +1,29 @@
-# Review:
-# Create a function called greet().
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
-
-def greet():
-    print("What is your name ?")
-    print("How old are you ?")
-    print("How do you do ?")
-
-greet()
-
-def greet_with_name(name):
-    print(f"Hi {name}")
-    print(f"How old are you, {name} ?")
-    print(f"How do you do ?")
-
-greet_with_name("Wale")
-
-
-def greet_with(name,location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}?")
-
-greet_with("Waleda", "Aberdeen")
+# # Review:
+# # Create a function called greet().
+# # Write 3 print statements inside the function.
+# # Call the greet() function and run your code.
+#
+# def greet():
+#     print("What is your name ?")
+#     print("How old are you ?")
+#     print("How do you do ?")
+#
+# greet()
+#
+# def greet_with_name(name):
+#     print(f"Hi {name}")
+#     print(f"How old are you, {name} ?")
+#     print(f"How do you do ?")
+#
+# greet_with_name("Wale")
+#
+#
+# def greet_with(name,location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}?")
+#
+# greet_with("Waleda", "Aberdeen")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# for [i] in alphabet:
+#     print([1])
+print(alphabet[1])
